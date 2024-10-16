7a76af3638d0584f1d1b1be1199e4047551f1063
unknown <184706@BME.HU>
2024-10-16 17:24:01 +0200

    egy valtoztatas a kodban

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -28,7 +28,7 @@ public class TorpedoStore {
     }
   }
 
-  private Random generator = new Random();
+  Random generator = new Random();
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
